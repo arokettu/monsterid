441b5180a054266b8c7c8542ffe5ba83ea50562e
Anton Smirnov <sandfox@sandfox.im>
2015-10-30 04:09:38 +0300

    comments in example

--- example/example.php
+++ example/example.php
@@ -2,8 +2,11 @@
 
 require __DIR__ . '/../src/monsterid.php';
 
+// use function is available for PHP >= 5.6, call with full namespace in earlier versions
 use function \SandFoxIM\MonsterID\build_monster;
 
+// make me an avatar
 $image = build_monster('sandfox@sandfox.im');
 
+// save it to file
 file_put_contents('avatar.png', $image);
