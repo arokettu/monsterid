9f7def53dfaf723e5f10c99bbe58a99632f628ee
Anton Smirnov <sandfox@sandfox.me>
2017-12-12 06:01:21 +0200

    Move logic parts to functions

--- src/classes/Monster.php
+++ src/classes/Monster.php
@@ -13,37 +13,37 @@ class Monster
         $this->seed = null;
 
         if ($seed) {
-            list($intSeed) = unpack('l',
+            // first index of unpack is 1
+            list(/* $_ */, $intSeed) = unpack('l',
                 substr(
                     md5($seed, true),   // raw md5 hash
                     0, 4                // 4 bytes
                 )
             );                          // convert to 32bit signed integer
+
             $this->seed = $intSeed;
         }
     }
 
     public function build($size = null)
     {
-        $seed = $this->seed;
+        $this->initRandom();
 
-        $PARTS_PATH = self::getPartsPath();
+        $parts      = $this->generateRandomParts();
+        $monster    = $this->createImage();
 
-        // init random seed
-        if ($seed) {
-            srand($seed);
+        // add parts
+        foreach ($parts as $part => $number) {
+            $this->applyPartToImage($monster, $part, $number);
         }
 
-        // throw the dice for body parts
-        $parts = array(
-            'legs' => rand(1, 5),
-            'hair' => rand(1, 5),
-            'arms' => rand(1, 5),
-            'body' => rand(1, 15),
-            'eyes' => rand(1, 15),
-            'mouth' => rand(1, 10)
-        );
+        $this->restoreRandom();
 
+        return $this->prepareOutput($monster, $size);
+    }
+
+    private function createImage()
+    {
         // create background
         $monster = imagecreatetruecolor(120, 120);
         if (!$monster) {
@@ -52,49 +52,78 @@ class Monster
         $white = imagecolorallocate($monster, 255, 255, 255);
         imagefill($monster, 0, 0, $white);
 
-        // add parts
-        foreach ($parts as $part => $num) {
-            $file = "{$PARTS_PATH}/{$part}_{$num}.png";
+        return $monster;
+    }
 
-            $im = imagecreatefrompng($file);
-            if (!$im) {
-                throw new PartNotLoadedException('Failed to load ' . $file);
-            }
-            imagesavealpha($im, true);
-            imagecopy($monster, $im, 0, 0, 0, 0, 120, 120);
-            imagedestroy($im);
+    private function applyPartToImage($image, $part, $number)
+    {
+        $file = implode(DIRECTORY_SEPARATOR, array(self::getPartsPath(), "{$part}_{$number}.png"));
 
-            // color the body
-            if ($part == 'body') {
-                $color = imagecolorallocate($monster, rand(20, 235), rand(20, 235), rand(20, 235));
-                imagefill($monster, 60, 60, $color);
-            }
+        $partImage = imagecreatefrompng($file);
+        if (!$partImage) {
+            throw new PartNotLoadedException('Failed to load ' . $file);
         }
+        imagesavealpha($partImage, true);
+        imagecopy($image, $partImage, 0, 0, 0, 0, 120, 120);
+        imagedestroy($partImage);
 
-        // restore random seed
-        if ($seed) {
-            srand();
+        // color the body
+        if ($part == 'body') {
+            $color = imagecolorallocate($image, rand(20, 235), rand(20, 235), rand(20, 235));
+            imagefill($image, 60, 60, $color);
         }
+    }
 
+    private function prepareOutput($image, $size)
+    {
         // resize if needed, then output
         if ($size && $size < 400) {
             $out = imagecreatetruecolor($size, $size);
             if (!$out) {
                 throw new ImageNotCreatedException('GD image create failed');
             }
-            imagecopyresampled($out, $monster, 0, 0, 0, 0, $size, $size, 120, 120);
+            imagecopyresampled($out, $image, 0, 0, 0, 0, $size, $size, 120, 120);
 
-            imagedestroy($monster);
+            imagedestroy($image);
         } else {
-            $out = $monster;
+            $out = $image;
         }
 
         ob_start();
         imagepng($out);
-        $image = ob_get_clean();
+        $buffer = ob_get_clean();
         imagedestroy($out);
 
-        return $image;
+        return $buffer;
+    }
+
+    private function initRandom()
+    {
+        // init random seed
+        if ($this->seed !== false) {
+            srand($this->seed);
+        }
+    }
+
+    private function restoreRandom()
+    {
+        // restore random seed
+        if ($this->seed !== false) {
+            srand();
+        }
+    }
+
+    private function generateRandomParts()
+    {
+        // throw the dice for body parts
+        return array(
+            'legs' =>   rand(1, 5),
+            'hair' =>   rand(1, 5),
+            'arms' =>   rand(1, 5),
+            'body' =>   rand(1, 15),
+            'eyes' =>   rand(1, 15),
+            'mouth' =>  rand(1, 10),
+        );
     }
 
     private static function getPartsPath()
