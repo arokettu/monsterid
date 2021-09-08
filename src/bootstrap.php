5095f952316c78cc39f4a9798c108d8060c346e8
Anton Smirnov <sandfox@sandfox.me>
2021-09-08 23:34:25 +0300

    $seed -> $string in functions

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -7,38 +7,38 @@ namespace SandFox\MonsterID;
 const MONSTER_DEFAULT_SIZE = 120; // same as image parts size
 
 /**
- * @param string|null $seed Any string id like email or openid
+ * @param string|null $string Any string id like email or openid
  * @param int $size Image size (square size x size)
  * @return string PNG image content
  */
-function build_monster(?string $seed = null, int $size = MONSTER_DEFAULT_SIZE): string
+function build_monster(?string $string = null, int $size = MONSTER_DEFAULT_SIZE): string
 {
-    $monster = new Monster($seed, $size);
+    $monster = new Monster($string, $size);
 
     return $monster->getImage();
 }
 
 /**
  * @param resource $stream Stream resource, PNG will be written there
- * @param string|null $seed Any string id like email or openid
+ * @param string|null $string Any string id like email or openid
  * @param int $size Image size (square size x size)
  * @return resource The same stream as $stream
  */
-function stream_monster($stream, ?string $seed = null, int $size = MONSTER_DEFAULT_SIZE)
+function stream_monster($stream, ?string $string = null, int $size = MONSTER_DEFAULT_SIZE)
 {
-    $monster = new Monster($seed, $size);
+    $monster = new Monster($string, $size);
 
     return $monster->writeToStream($stream);
 }
 
 /**
- * @param string|null $seed Any string id like email or openid
+ * @param string|null $string Any string id like email or openid
  * @param int $size Image size (square size x size)
  * @return \GdImage|resource GD object
  */
-function build_monster_gd(?string $seed = null, int $size = MONSTER_DEFAULT_SIZE)
+function build_monster_gd(?string $string = null, int $size = MONSTER_DEFAULT_SIZE)
 {
-    $monster = new Monster($seed, $size);
+    $monster = new Monster($string, $size);
 
     return $monster->getGdImage();
 }
