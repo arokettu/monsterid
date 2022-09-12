a386c55e643469d14931ad671f44b14f7bbaa14f
Anton Smirnov <sandfox@sandfox.me>
2022-09-13 00:18:54 +0300

    Useless code

--- src/classes/Randomizer/DefaultV1Factory.php
+++ src/classes/Randomizer/DefaultV1Factory.php
@@ -10,11 +10,7 @@ final class DefaultV1Factory implements FactoryInterface
 {
     public function getRandomizer(string $seed): Engine
     {
-        [/* $_ */, $intSeed] = unpack('l', substr(
-            md5($seed, true),   // raw md5 hash
-            0,
-            4                   // 4 bytes
-        ));                     // convert to 32bit signed integer
+        [/* $_ */, $intSeed] = unpack('l', md5($seed, true)); // convert to 32bit signed integer
 
         // As v1.3.0 generated on PHP 7.2+
         return new Engine\Mt19937($intSeed, \MT_RAND_MT19937);
