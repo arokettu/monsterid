4c83ce323d3ef2c1a19d89d2c942b49e8cc28ba7
Anton Smirnov <sandfox@sandfox.me>
2023-12-13 07:25:21 +0200

    Update comment

--- src/classes/Randomizer/DefaultV0Factory.php
+++ src/classes/Randomizer/DefaultV0Factory.php
@@ -13,7 +13,8 @@ final class DefaultV0Factory implements FactoryInterface
         [/*$_*/, $byte1, $byte2, $byte3] = unpack("C3", md5($seed, true));
         $intSeed = $byte1 << 16 | $byte2 << 8 | $byte3;
 
-        // As v1.3.0 generated on PHP 7.2+
+        // As the original implementation generated on PHP 7.2+
+        // https://github.com/splitbrain/monsterID
         return new Engine\Mt19937($intSeed, \MT_RAND_MT19937);
     }
 }
