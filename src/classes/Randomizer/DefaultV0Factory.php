544593caac206eb729d07952d36b2f777888fc99
Anton Smirnov <sandfox@sandfox.me>
2023-12-13 07:50:12 +0200

    Unpack a single value

--- src/classes/Randomizer/DefaultV0Factory.php
+++ src/classes/Randomizer/DefaultV0Factory.php
@@ -13,8 +13,8 @@ final class DefaultV0Factory implements FactoryInterface
         // original code was:
         // $intSeed = hexdec(substr(md5($seed),0,6));
 
-        [/*$_*/, $byte1, $byte2, $byte3] = unpack("C3", md5($seed, true));
-        $intSeed = $byte1 << 16 | $byte2 << 8 | $byte3;
+        ['val' => $val] = unpack("Nval", md5($seed, true));
+        $intSeed = ($val >> 8) & 0xffffff;
 
         // As the original implementation generated on PHP 7.2+
         // https://github.com/splitbrain/monsterID
